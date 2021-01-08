8e6ed8deda3d76ef4296a26a2b090dd92b739a00
Jake Tovsen <jacobtovsen@gmail.com>
2021-01-08 16:24:34 -0600

    functions

--- Tinkering.js
+++ Tinkering.js
@@ -7,12 +7,20 @@
 console.log('hello world');
 
 let a = 0;
+console.log(a);
 
 let b = 1;
+console.log(b);
 
 let c = 2;
+console.log(c);
 
 console.log(a+b+c);
 
 
+function addition(num1, num2){
+    console.log( (num1 + num2) / 2 );
+}
 
+addition (2, 5);
+addition (11, 9);
